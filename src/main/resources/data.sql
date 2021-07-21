87f0bc1d2f9500edfa8b0d7d04d04c68903884be
iwan morgan <caci_iwan.morgan@titania.com>
2021-07-21 09:01:46 +0100

    Added database tables and data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,16 @@
+DELETE FROM Ingredient_Ref;
+DELETE FROM Taco;
+DELETE FROM Taco_Order;
+DELETE FROM Ingredient;
+
+INSERT INTO Ingredient (id, name, type)
+  VALUES  ('FLTO', 'Flour Tortilla', 'WRAP'),
+          ('COTO', 'Corn Tortilla', 'WRAP'),
+          ('GRBF', 'Ground Beef', 'PROTEIN'),
+          ('CARN', 'Carnitas', 'PROTEIN'),
+          ('TMTO', 'Diced Tomatoes', 'VEGGIES'),
+          ('LETC', 'Lettuce', 'VEGGIES'),
+          ('CHED', 'Cheddar', 'CHEESE'),
+          ('JACK', 'Monterrey Jack', 'CHEESE'),
+          ('SLSA', 'Salsa', 'SAUCE'),
+          ('SRCR', 'Sour Cream', 'SAUCE');
